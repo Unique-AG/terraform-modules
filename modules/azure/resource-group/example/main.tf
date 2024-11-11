a4eb3ea9fe6c3106c313e1f94050e7abdfe30d2a
Amine Kahlaoui <amine.tity@live.fr>
2024-11-11 10:29:24 +0100

    chore: add pre-commit hooks (#2)
    - adds pre-commit hooks
    - adds tf-sec
    - adds automatic terraform-docs generation
    
    ---------
    
    Co-authored-by: Dominik from Unique <dominik@unique.ch>

---
+++ modules/azure/resource-group/example/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_version = ">= 1.9.7"
+  required_providers {
+    # azurerm = {
+    #   source  = "hashicorp/azurerm"
+    #   version = "3.115.0"
+    # }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.3"
+    }
+  }
+
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
+
+# provider "azurerm" {
+#   features {}
+# }
+
+provider "null" {}
+
+
+module "rg" {
+  source = "../"
+  resource_groups = {
+    rg1 = {
+      name     = "rg1"
+      location = "eastus"
+      tags = {
+        environment = "dev"
+      }
+    }
+    rg2 = {
+      name     = "rg2"
+      location = "westus"
+      tags = {
+        environment = "dev"
+      }
+    }
+  }
+}
