d0d57fac43cfceb2c914f24eb8a6cf16642349cc
unique-serghei <serghei@unique.ch>
2024-12-02 10:52:07 +0100

    feat: add expiration_date to connection_settings for storage account secrets

--- modules/azure-storage-account/variables.tf
+++ modules/azure-storage-account/variables.tf
@@ -161,6 +161,7 @@ variable "connection_settings" {
     connection_string_1 = string
     connection_string_2 = string
     key_vault_id        = string
+    expiration_date     = optional(string, "9999-12-31T23:59:59Z")
   })
   default  = null
   nullable = true
