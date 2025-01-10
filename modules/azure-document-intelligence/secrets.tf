737d747d870b4b83237794a748d9fb4090dcbf5d
unique-jakub <jakub@uniqe.ch>
2025-01-10 15:28:47 +0100

    use variable to determine whether to store secrets

--- modules/azure-document-intelligence/secrets.tf
+++ modules/azure-document-intelligence/secrets.tf
@@ -3,14 +3,14 @@ locals {
 }
 
 resource "azurerm_key_vault_secret" "key" {
-  for_each     = var.key_vault_id != null ? azurerm_cognitive_account.aca : []
+  for_each     = local.create_vault_secrets ? azurerm_cognitive_account.aca : []
   name         = "${each.key}${var.primary_access_key_secret_name_suffix}"
   value        = each.value.primary_access_key
   key_vault_id = var.key_vault_id
 }
 
 resource "azurerm_key_vault_secret" "azure_document_intelligence_endpoints" {
-  count        = var.key_vault_id != null ? 1 : 0
+  count        = local.create_vault_secrets ? 1 : 0
   name         = var.endpoints_secret_name
   value        = jsonencode(local.azure_document_intelligence_endpoints)
   key_vault_id = var.key_vault_id
