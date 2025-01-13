06d2b9fee53dea27a2dbd804a8188ffef82b260c
amine7777 <amine.kahlaoui@aol.com>
2025-01-13 11:18:39 +0100

    Edit values for the secrets

--- modules/azure-oai/secrets.tf
+++ modules/azure-oai/secrets.tf
@@ -5,7 +5,7 @@ locals {
 resource "azurerm_key_vault_secret" "primary_access_keys" {
   for_each     = local.create_vault_secrets ? azurerm_cognitive_account.aca : {}
   name         = "${basename(each.value.id)}${var.primary_access_key_secret_name_suffix}"
-  value        = each.value.primary_access_key
+  value        = try(each.value.primary_access_key, "")
   key_vault_id = var.key_vault_id
 }
 
