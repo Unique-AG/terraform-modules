f812bda2caf10bc4650f95b84c513c0a06e792d8
unique-jakub <jakub@unique.ch>
2025-01-13 17:58:49 +0100

    fix(openai): azure openai secrets (#12)
    ## Describe your changes
    
    <!-- Describe your changes in detail -->
    
    ## Checklist before requesting a review
    - [ ] Module version bumped (`module.yaml`)
    - [ ] Changelog updated (`module.yaml`)
    - [ ] Pre-Commit passed or has been run manually (`Makefile`)
    
    ---------
    
    Co-authored-by: unique-jakub <jakub@uniqe.ch>

--- modules/azure-openai/secrets.tf
+++ modules/azure-openai/secrets.tf
@@ -1,21 +1,25 @@
 locals {
   create_vault_secrets = var.key_vault_id != null
+  # Filtered  cognitive accounts to include only those with a local auth enabled
+  aca_with_local_auth = {
+    for k, v in var.cognitive_accounts : k => v
+    if v.local_auth_enabled
+  }
 }
 
-resource "azurerm_key_vault_secret" "primary_access_keys" {
-  for_each     = local.create_vault_secrets ? azurerm_cognitive_account.aca : {}
-  name         = "${basename(each.value.id)}${var.primary_access_key_secret_name_suffix}"
-  value        = try(each.value.primary_access_key, "")
+resource "azurerm_key_vault_secret" "key" {
+  for_each     = local.create_vault_secrets ? local.aca_with_local_auth : {}
+  name         = "${each.key}${var.primary_access_key_secret_name_suffix}"
+  value        = azurerm_cognitive_account.aca[each.value.name].primary_access_key
   key_vault_id = var.key_vault_id
-  depends_on   = [azurerm_cognitive_account.aca]
 }
 
+# Store the endpoint for each cognitive account in Key Vault
 resource "azurerm_key_vault_secret" "endpoint" {
   for_each     = local.create_vault_secrets ? azurerm_cognitive_account.aca : {}
-  name         = "${basename(each.value.id)}-endpoint"
+  name         = "${each.key}${var.endpoint_secret_name_suffix}"
   value        = each.value.endpoint
   key_vault_id = var.key_vault_id
-  depends_on   = [azurerm_cognitive_account.aca]
 }
 
 resource "azurerm_key_vault_secret" "model_version_endpoints" {
@@ -23,5 +27,4 @@ resource "azurerm_key_vault_secret" "model_version_endpoints" {
   name         = var.endpoint_definitions_secret_name
   value        = jsonencode(local.model_version_endpoints)
   key_vault_id = var.key_vault_id
-  depends_on   = [azurerm_cognitive_account.aca]
 }
