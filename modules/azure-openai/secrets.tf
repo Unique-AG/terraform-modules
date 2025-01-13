7a3eb140a05834123a5a58b8541639489dff6ef9
unique-jakub <jakub@uniqe.ch>
2025-01-13 17:30:23 +0100

    fix the secret

--- modules/azure-openai/secrets.tf
+++ modules/azure-openai/secrets.tf
@@ -2,21 +2,23 @@ locals {
   create_vault_secrets = var.key_vault_id != null
   # Filtered  cognitive accounts to include only those with a local auth enabled
   aca_with_local_auth = {
-    for k, v in azurerm_cognitive_account.aca : k => v
-    if v.primary_access_key != ""
+    for k, v in var.cognitive_accounts : k => v
+    if v.local_auth_enabled
   }
+
+
 }
 
 resource "azurerm_key_vault_secret" "key" {
-  for_each        = local.create_vault_secrets ? azurerm_cognitive_account.aca : {}
+  for_each        = local.create_vault_secrets ? local.aca_with_local_auth : {}
   name            = "${each.key}${var.primary_access_key_secret_name_suffix}"
-  value           = each.value.primary_access_key
+  value           = azurerm_cognitive_account.aca[each.value.name].primary_access_key
   key_vault_id    = var.key_vault_id
 }
 
 # Store the endpoint for each cognitive account in Key Vault
 resource "azurerm_key_vault_secret" "endpoint" {
-  for_each        = local.create_vault_secrets ? local.aca_with_local_auth : {}
+  for_each        = local.create_vault_secrets ? azurerm_cognitive_account.aca : {}
   name            = "${each.key}${var.endpoint_secret_name_suffix}"
   value           = each.value.endpoint
   key_vault_id    = var.key_vault_id
