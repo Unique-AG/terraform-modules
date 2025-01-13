ca1659edff4091e89e17d635ef8b8a17e3686370
unique-jakub <jakub@unique.ch>
2025-01-13 15:21:03 +0100

    feat: add azure oai module (#6)
    ## Describe your changes
    
    <!-- Describe your changes in detail -->
    
    ## Checklist before requesting a review
    - [ ] Module version bumped (`module.yaml`)
    - [ ] Changelog updated (`module.yaml`)
    - [ ] Pre-Commit passed or has been run manually (`Makefile`)
    
    ---------
    
    Co-authored-by: unique-jakub <jakub@uniqe.ch>
    Co-authored-by: Sandro Camastral <sandro@unique.ch>
    Co-authored-by: amine7777 <amine.kahlaoui@aol.com>

---
+++ modules/azure-oai/main.tf
@@ -0,0 +1,67 @@
+locals {
+  model_version_endpoints = [
+    for account in azurerm_cognitive_account.aca : {
+      "endpoint" : account.endpoint,
+      "location" : account.location,
+      "models" : [
+        for deployment in azurerm_cognitive_deployment.deployments : {
+          "modelName" : deployment.model[0].name,
+          "deploymentName" : deployment.name,
+          "modelVersion" : deployment.model[0].version
+        } if deployment.cognitive_account_id == account.id
+      ]
+    }
+  ]
+
+  flattened_deployments = flatten([
+    for account_key, account in var.cognitive_accounts : [
+      for deployment in account.cognitive_deployments : {
+        account_key            = account_key
+        name                   = deployment.name
+        model_name             = deployment.model_name
+        model_version          = deployment.model_version
+        model_format           = deployment.model_format
+        sku_capacity           = deployment.sku_capacity
+        sku_type               = deployment.sku_type
+        custom_subdomain_name  = deployment.custom_subdomain_name
+        rai_policy_name        = deployment.rai_policy_name
+        version_upgrade_option = deployment.version_upgrade_option
+      }
+    ]
+  ])
+}
+
+resource "azurerm_cognitive_account" "aca" {
+  for_each                      = var.cognitive_accounts
+  name                          = each.value.name
+  location                      = each.value.location
+  resource_group_name           = var.resource_group_name
+  kind                          = each.value.kind
+  sku_name                      = each.value.sku_name
+  tags                          = var.tags
+  public_network_access_enabled = each.value.public_network_access_enabled
+  local_auth_enabled            = each.value.local_auth_enabled
+  custom_subdomain_name         = each.value.custom_subdomain_name
+}
+
+resource "azurerm_cognitive_deployment" "deployments" {
+
+  for_each = {
+    for deployment in local.flattened_deployments : "${deployment.account_key}-${deployment.name}" => deployment
+  }
+  name                   = each.value.name
+  cognitive_account_id   = azurerm_cognitive_account.aca[each.value.account_key].id
+  rai_policy_name        = each.value.rai_policy_name
+  version_upgrade_option = each.value.version_upgrade_option
+
+  model {
+    format  = each.value.model_format
+    name    = each.value.model_name
+    version = each.value.model_version
+  }
+
+  sku {
+    name     = each.value.sku_type
+    capacity = each.value.sku_capacity
+  }
+}
