6f0e4f853a89538e0b33e0a05eec7041908ccaca
unique-jakub <jakub@uniqe.ch>
2025-01-13 16:22:25 +0100

    fix open ai created secrets

--- modules/azure-openai/secrets.tf
+++ modules/azure-openai/secrets.tf
@@ -4,18 +4,16 @@ locals {
 
 resource "azurerm_key_vault_secret" "primary_access_keys" {
   for_each     = local.create_vault_secrets ? azurerm_cognitive_account.aca : {}
-  name         = "${basename(each.value.id)}${var.primary_access_key_secret_name_suffix}"
-  value        = try(each.value.primary_access_key, "")
+  name         = "${each.key}${var.primary_access_key_secret_name_suffix}"
+  value        = each.value.primary_access_key
   key_vault_id = var.key_vault_id
-  depends_on   = [azurerm_cognitive_account.aca]
 }
 
-resource "azurerm_key_vault_secret" "endpoint" {
-  for_each     = local.create_vault_secrets ? azurerm_cognitive_account.aca : {}
-  name         = "${basename(each.value.id)}-endpoint"
-  value        = each.value.endpoint
+resource "azurerm_key_vault_secret" "cognitive_account_endpoints" {
+  count        = local.create_vault_secrets ? 1 : 0
+  name         = var.endpoints_secret_name
+  value        = { for aca in azurerm_cognitive_account.aca : aca.name => aca.endpoint }
   key_vault_id = var.key_vault_id
-  depends_on   = [azurerm_cognitive_account.aca]
 }
 
 resource "azurerm_key_vault_secret" "model_version_endpoints" {
@@ -23,5 +21,4 @@ resource "azurerm_key_vault_secret" "model_version_endpoints" {
   name         = var.endpoint_definitions_secret_name
   value        = jsonencode(local.model_version_endpoints)
   key_vault_id = var.key_vault_id
-  depends_on   = [azurerm_cognitive_account.aca]
 }
