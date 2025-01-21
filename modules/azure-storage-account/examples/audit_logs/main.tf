154b9497fee0baea72ba32f778248d05f958544e
Dominik Meyer <dominik@unique.ch>
2025-01-21 09:19:03 +0100

    chore(azure-storage-account): add full audit_logs example

--- modules/azure-storage-account/examples/audit_logs/main.tf
+++ modules/azure-storage-account/examples/audit_logs/main.tf
@@ -46,49 +46,54 @@ resource "azurerm_role_assignment" "kv_sens_storage_account_audit_logs_key_servi
 }
 
 module "sa" {
-  source              = "../.."
-  name                = "my-storage-account"
-  resource_group_name = "my-resource-group"
-  location            = "switzerlandnorth"
-  tags = {
-    environment = "example"
+  source = "../.."
+
+  container_deleted_retain_days = 7
+  deleted_retain_days           = 7
+  depends_on                    = [azurerm_role_assignment.kv_sens_storage_account_audit_logs_key_service_user, azurerm_key_vault_key.auditlogs_key]
+  is_nfs_mountable              = true
+  location                      = "switzerlandnorth"
+  name                          = "my-storage-account"
+  resource_group_name           = "my-resource-group"
+
+  containers = {
+    # service-one is only an example, list the necessary containers here for all services of which you want to store audit logs
+    service-one = {
+      access_type = "private"
+    }
+  }
+
+  customer_managed_key = {
+    key_name                  = azurerm_key_vault_key.auditlogs_key.name
+    key_vault_id              = azurerm_key_vault.sensitive.id
+    user_assigned_identity_id = azurerm_user_assigned_identity.storage_account_keyvault_key_reader_audit_logs.id
   }
 
   identity_ids = [
     azurerm_user_assigned_identity.storage_account_keyvault_key_reader_audit_logs.id
   ]
 
-  customer_managed_key = {
-    key_vault_id              = azurerm_key_vault.sensitive.id
-    key_name                  = azurerm_key_vault_key.auditlogs_key.name
-    user_assigned_identity_id = azurerm_user_assigned_identity.storage_account_keyvault_key_reader_audit_logs.id
-  }
-
-  depends_on = [azurerm_role_assignment.kv_sens_storage_account_audit_logs_key_service_user, azurerm_key_vault_key.auditlogs_key]
-
-  containers = {
-    service-one = {
-      access_type = "private"
-    }
-  }
-
-  storage_management_policy_default = {
-    enabled                                  = true
-    deleted_retain_days                      = 7
-    restorable_days                          = 6
-    container_deleted_retain_days            = 7
-    blob_to_cool_after_last_modified_days    = 1
-    blob_to_cold_after_last_modified_days    = 2
-    blob_to_archive_after_last_modified_days = 3
-    blob_to_deleted_after_last_modified_days = 1825 # 5 years
-  }
-
   network_rules = {
-    ip_rules                   = ["12.34.56.78/21"]
-    virtual_network_subnet_ids = ["/subnet/id"]
+    ip_rules = ["12.34.56.78/21"]
     private_link_accesses = [{
       endpoint_resource_id = "/sub/uuid/scanner"
       endpoint_tenant_id   = "58455a0c-c831-4f5a-b460-07156e44f4c2"
     }]
+    virtual_network_subnet_ids = ["/subnet/id"]
+  }
+
+  storage_management_policy_default = {
+    blob_to_archive_after_last_modified_days = 3
+    blob_to_cold_after_last_modified_days    = 2
+    blob_to_cool_after_last_modified_days    = 1
+    blob_to_deleted_after_last_modified_days = 1825 # 5 years
+    container_deleted_retain_days            = 7
+    deleted_retain_days                      = 7
+    enabled                                  = true
+    restorable_days                          = 6
+  }
+
+  tags = {
+    environment = "example"
   }
 }
