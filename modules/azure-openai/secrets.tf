53641d24bc72f316324eabf2b551c6963e72e3a4
amine7777 <amine.kahlaoui@aol.com>
2025-02-11 10:12:10 +0100

    Delete local.aca_with_local_auth variable as we would like to use the sensitive kv instead

--- modules/azure-openai/secrets.tf
+++ modules/azure-openai/secrets.tf
@@ -1,18 +1,12 @@
 locals {
-  create_vault_secrets      = var.key_vault_id != null
-  create_vault_secrets_sens = var.sens_key_vault_id != null
-  # Filtered  cognitive accounts to include only those with a local auth enabled
-  aca_with_local_auth = {
-    for k, v in var.cognitive_accounts : k => v
-    if v.local_auth_enabled
-  }
+  create_vault_secrets = var.key_vault_id != null
 }
 
 resource "azurerm_key_vault_secret" "key" {
-  for_each     = local.create_vault_secrets_sens ? local.aca_with_local_auth : {}
+  for_each     = local.create_vault_secrets ? azurerm_cognitive_account.aca : {}
   name         = "${each.key}${var.primary_access_key_secret_name_suffix}"
   value        = azurerm_cognitive_account.aca[each.value.name].primary_access_key
-  key_vault_id = var.sens_key_vault_id
+  key_vault_id = var.key_vault_id
 }
 
 # Store the endpoint for each cognitive account in Key Vault
