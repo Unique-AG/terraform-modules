12aea62d1a2659289b04fec97b907c155e350c94
Amine Kahlaoui <amine.tity@live.fr>
2025-02-11 11:36:19 +0100

    feat(platform): Revert changes of the previous version (#36)
    ## Describe your changes
    
    <!-- Describe your changes in detail -->
    
    ## Checklist before requesting a review
    - [x] Module version bumped (`module.yaml`)
    - [x] Changelog updated (`module.yaml`)
    - [x] Pre-Commit passed or has been run manually (`Makefile`)

--- modules/azure-openai/secrets.tf
+++ modules/azure-openai/secrets.tf
@@ -1,9 +1,14 @@
 locals {
   create_vault_secrets = var.key_vault_id != null
+  # Filtered  cognitive accounts to include only those with a local auth enabled
+  aca_with_local_auth = {
+    for k, v in var.cognitive_accounts : k => v
+    if v.local_auth_enabled
+  }
 }
 
 resource "azurerm_key_vault_secret" "key" {
-  for_each     = local.create_vault_secrets ? azurerm_cognitive_account.aca : {}
+  for_each     = local.create_vault_secrets ? local.aca_with_local_auth : {}
   name         = "${each.key}${var.primary_access_key_secret_name_suffix}"
   value        = azurerm_cognitive_account.aca[each.value.name].primary_access_key
   key_vault_id = var.key_vault_id
