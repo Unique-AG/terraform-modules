7b473d0cefcbddc203d14ba1ba31c1a35fa7f12a
unique-dominik <dominik@unique.ch>
2025-04-01 13:44:07 +0200

    fix(trivy): upload sarif files

--- scripts/trivy.sh
+++ scripts/trivy.sh
@@ -4,4 +4,17 @@
 # SPDX-SnippetCopyrightText: 2024 © Unique AG
 # SPDX-SnippetEnd
 echo "Running Trivy for Terraform scanning"
-docker run --rm -v "$(pwd):/workdir" aquasec/trivy config --ignorefile /workdir/.github/configs/trivyignore.yaml /workdir
+
+CMD="docker run --rm -v "$(pwd):/workdir" aquasec/trivy config --ignorefile /workdir/.github/configs/trivyignore.yaml"
+
+# Check if output file path is provided as argument
+if [ -n "$1" ]; then
+  TRIVY_OUTPUT_FILE="/workdir/$1"
+  touch "$TRIVY_OUTPUT_FILE"
+  chmod 666 "$TRIVY_OUTPUT_FILE"
+  CMD="$CMD --format sarif -o $TRIVY_OUTPUT_FILE"
+fi
+
+CMD="$CMD /workdir"
+
+eval "$CMD"
