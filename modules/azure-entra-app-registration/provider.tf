23f68ec70b6150ca8b11c28cd1991e4e02a13447
unique-dominik <dominik@unique.ch>
2025-04-14 11:50:34 +0200

    feat(app-reg): make KV storage optional

--- modules/azure-entra-app-registration/provider.tf
+++ modules/azure-entra-app-registration/provider.tf
@@ -3,9 +3,8 @@ terraform {
   required_version = ">= 1.5"
   required_providers {
     azurerm = {
-      source   = "hashicorp/azurerm"
-      version  = "~> 4.15"
-      features = {}
+      source  = "hashicorp/azurerm"
+      version = "~> 4.15"
     }
     azuread = {
       source  = "hashicorp/azuread"
