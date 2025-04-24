48da5bbd468229c60c7a9b9e43533c2281fd9993
unique-dominik <dominik@unique.ch>
2025-04-24 10:59:58 +0200

    feat(aks): output csi identity

--- modules/azure-kubernetes-service/outputs.tf
+++ modules/azure-kubernetes-service/outputs.tf
@@ -9,7 +9,7 @@ output "kubernetes_node_rg_name" {
 }
 
 output "csi_user_assigned_identity_name" {
-  description = "The name of the user-assigned identity for the CSI driver."
+  description = "The name of the user-assigned identity for the CSI driver. Prefer using the csi_identity_client_id and csi_identity_object_id outputs as they are more reliable."
   value       = "azurekeyvaultsecretsprovider-${azurerm_kubernetes_cluster.cluster.name}"
 }
 
@@ -31,4 +31,14 @@ output "agic_identity_client_id" {
 output "agic_identity_object_id" {
   description = "The object ID of the identity used by the Application Gateway Ingress Controller."
   value       = azurerm_kubernetes_cluster.cluster.ingress_application_gateway[0].ingress_application_gateway_identity[0].object_id
+}
+
+output "csi_identity_client_id" {
+  description = "The client ID of the identity used by the CSI driver."
+  value       = azurerm_kubernetes_cluster.cluster.key_vault_secrets_provider[0].secret_identity[0].client_id
+}
+
+output "csi_identity_object_id" {
+  description = "The object ID of the identity used by the CSI driver."
+  value       = azurerm_kubernetes_cluster.cluster.key_vault_secrets_provider[0].secret_identity[0].object_id
 }
