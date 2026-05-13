a4a47ff5656f7f0ee73915cdfd26ea65aaf45428
unique-jakub <jakub@unique.ch>
2026-05-13 13:32:48 +0200

    style(azure-kubernetes-service): terraform fmt provider.tf

--- modules/azure-kubernetes-service/provider.tf
+++ modules/azure-kubernetes-service/provider.tf
@@ -3,7 +3,7 @@ terraform {
   required_version = ">= 1.12"
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
+      source = "hashicorp/azurerm"
       # 4.43+ required for upgrade_settings.undrainable_node_behavior
       version = "~> 4.43"
     }
